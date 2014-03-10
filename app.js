0e0f480343aba83d1acf53636c66f773e43244b0
uzairbukhari <suzairbukhari@gmail.com>
2014-03-10 16:27:16 +0500

    added username functionality

---
+++ app.js
@@ -0,0 +1,17 @@
+﻿jQuery(function ($) {
+    var socket = io.connect();
+    var $msgForm = $('#sendMsg');
+    var $msgBox = $('#chatDiv');
+    var $msg = $('#txtMessage');
+
+    $msgForm.submit(function (e) {
+        e.preventDefault();
+        socket.emit('send message', $msg.val());
+        $msg.val('');
+    });
+
+    socket.on('new message', function (data) {
+        var html = '<div class="chat-text"><b>Uzair : </b> ' + data + '</div>';
+        $msgBox.append(html);
+    });
+});
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 ﻿var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
-    io = require('socket.io').listen(server);
+    io = require('socket.io').listen(server),
+    users = [];
 
 server.listen(3000);
 
@@ -10,8 +11,33 @@ app.get('/', function(req , res) {
 });
 
 io.sockets.on('connection', function(socket) {
-    socket.on('send message', function(data) {
-        io.sockets.emit('new message', data);
+    socket.on('new user', function (data , callback) {
+        if (users.indexOf(data) != -1) {
+            callback(false);
+        } else {
+            callback(true);
+            socket.user = data;
+            users.push(socket.user);
+            updateUserList();
+        }
+    });
+
+    socket.on('send message', function (data) {
+        io.sockets.emit('new message', {
+            name: socket.user,
+            msg : data
+        });
         //socket.broadcast.emit('new message', data); // send to everyone except me
     });
+
+    socket.on('disconnect', function(data) {
+        if (!socket.user) return;
+
+        users.splice(users.indexOf(socket.user), 1);
+        updateUserList();
+    });
+
+    function updateUserList() {
+        io.sockets.emit('usernames', users);
+    }
 })
